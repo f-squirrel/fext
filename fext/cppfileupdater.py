cfe33a1cedcb470a1ea2fe5642791316e742da52
f-squirrel <dmitry.b.danilov@gmail.com>
2021-11-10 11:03:38 +0200

    feat(cpp-creator): draft

---
+++ fext/cppfileupdater.py
@@ -0,0 +1,25 @@
+from fext import nodefilter
+from clang.cindex import CursorKind
+from string import Template
+import clang.cindex
+import os
+
+
+class CppFileUpdater:
+    def __init__(self, header_root, filename):
+        self._header_root = header_root
+        self._cpp_root = nodefilter.NodeFilter(filename=filename).filter()
+        with open(filename, "r") as f:
+            self._cpp_content = f.read()
+        index = clang.cindex.Index.create()
+        translation_unit = index.parse(filename, args=['-std=c++17'])
+    
+    def update(self):
+        pass
+
+    def build(self):
+        return self._build()
+    
+    def _build(self):
+        pass
+
