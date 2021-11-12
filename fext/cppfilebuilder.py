98b3336286aea0765ad37136459aabd7c7080c30
f-squirrel <dmitry.b.danilov@gmail.com>
2021-11-13 00:38:09 +0200

    refactor: update types in cpp builder

--- fext/cppfilebuilder.py
+++ fext/cppfilebuilder.py
@@ -1,17 +1,19 @@
+from fext.node import Node
+from clang.cindex import Cursor
 from clang.cindex import CursorKind
 from string import Template
 import os
 
 
 class CppFileBuilder:
-    def __init__(self, root, header_content):
+    def __init__(self, root: Node, header_content: str):
         self._header_content = header_content
         self._root= root
 
-    def build(self):
+    def build(self) -> str:
         return self._build(self._root)
 
-    def _build(self, node):
+    def _build(self, node: Node) -> str:
         output_string = ""
 
         for c in node.children:
@@ -42,23 +44,23 @@ class CppFileBuilder:
             return Template(output_string).substitute(
                 parent="${{parent}}{}::".format(node.cursor.displayname))
 
-    def _get_body(self, cursor):
+    def _get_body(self, cursor: Cursor) -> Cursor:
         for c in cursor.get_children():
             if c.kind == CursorKind.COMPOUND_STMT:
                 return c
         return None
 
-    def _extract_body(self, cursor):
+    def _extract_body(self, cursor: Cursor) -> str:
         body = self._get_body(cursor)
         return self._header_content[body.extent.start.offset: body.extent.end.offset]
 
-    def _extract_args(self, cursor):
+    def _extract_args(self, cursor: Cursor) -> str:
         args = []
         for arg in cursor.get_arguments():
             args.append("{} {}".format(arg.type.spelling, arg.spelling))
         return ", ".join(args)
 
-    def _build_method(self, cursor):
+    def _build_method(self, cursor: Cursor) -> str:
         const_tag = "const " if cursor.is_const_method() else ""
 
         output_string = "\n{return_type} ${{parent}}{name}({args}) {const_tag}{body}\n".format(
@@ -69,7 +71,7 @@ class CppFileBuilder:
                 body=self._extract_body(cursor))
         return output_string
 
-    def _build_function(self, cursor):
+    def _build_function(self, cursor: Cursor) -> str:
         output_string = "\n{return_type} ${{parent}}{name}({args}) {body}\n".format(
                 return_type=cursor.result_type.spelling,
                 name=cursor.spelling,
